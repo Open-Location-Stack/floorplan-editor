924ff6e7268799634cd887ab57d0012671f181ef
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2026-02-06 15:19:59 +0100

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  build: {
+    target: "esnext",
+    modulePreload: {
+      polyfill: false,
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "esnext",
+    },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+  },
+});
